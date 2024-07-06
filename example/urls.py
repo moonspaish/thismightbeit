ea079a38eabf3d4e67034224c4de5cb748bb54fd
moonspaish <69521713+moonspaish@users.noreply.github.com>
2024-07-06 14:44:12 +0300

    kinda added resume page

--- example/urls.py
+++ example/urls.py
@@ -3,4 +3,7 @@ from example import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path('projects/', views.projects, name='projects'),  # Projects view
+    path('about/', views.about, name='about'),  # About view
+    path('cv/', views.cv, name='cv'),
 ]
--- example/views.py
+++ example/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "example/index.html", {})
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def cv(request):
+    return render(request, 'example/cv.html')
